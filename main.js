6f23513a79251765fa30859da71e655f6665fc0f
luanaribeiro2007 <104360850+luanaribeiro2007@users.noreply.github.com>
2023-11-08 13:52:55 -0300

    0811

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-function tocaSom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
 while(contador <listaDeTeclas.length){
-    listaDeTeclas[0].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#som_tecla_splash')
+    };
     contador = contador + 1;
 
     console.log(contador);
